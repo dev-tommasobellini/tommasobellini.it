d3252896a9b3b6a1704ce747c683c4acdd7c0682
Tommaso Bellini <tommaso.bellini@serioplast.com>
2019-03-01 00:01:31 +0100

    name changed

--- tommasobellini-front/src/components/FirstSection.vue
+++ tommasobellini-front/src/components/FirstSection.vue
@@ -4,7 +4,7 @@
     <header class="arrow-down">
       <div class="text-in-arrow">
         <h1 class="title">
-          <span id="name">Tommaso  Martino25</span>
+          <span id="name">Tommaso</span>
           <span id="lastname">Bellini</span>
         </h1>
          <h3 class="job">Software Developer</h3>
