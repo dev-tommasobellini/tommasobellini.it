f476aa907046e5b3a5cd1f132abc4d4d15a919cf
tommaso bellini <tommasobellini@MacBook-Air-di-tommaso.local>
2020-01-10 14:16:11 +0100

    added messages

--- tommasobellini-front/deploy.sh
+++ tommasobellini-front/deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm.cmd run build
+npm run build
 
 # deploy to firebase
-firebase deploy --token "1/Ih83f_4ydAhZYQ44hVgyKieOB-aFw9nWQQb-taSdY2c"
+firebase deploy
--- tommasobellini-front/src/components/SecondSection.vue
+++ tommasobellini-front/src/components/SecondSection.vue
@@ -1,38 +1,72 @@
 <template>
-    <div class="secondSectionContainer">
-        <div class="title">
-            <h2>About me</h2>
-        </div>
-        <div class="profile">
-            <img class="profileImg" src="../assets/MyProfilePhoto.jpeg"/>
-        </div>
-        <div class="stories">
-            <div class="borderLeft">
-                <h4>My personal Story</h4>
-                <p>
-                    Hi, I'm Tommaso and i'm 22 years old.
-                    I live in Italy, more specifically near Bergamo.
-                    I was born in Warsaw (Poland) and i was adopted when i was 5 years old by two italians.
-                </p>
-            </div>
-            <div class="textAlignRight borderTop borderRight">
-                <h4>My school studies and work Story</h4>
-                <p>
-                    Hi, I'm Tommaso and i'm 22 years old.
-                    I live in Italy, more specifically near Bergamo.
-                    I was born in Warsaw (Poland) and i was adopted when i was 5 years old by two italians.
-                </p>
-            </div>
-             <div class="borderTop borderLeft">
-                <h4>My future goals Story</h4>
-                <p>
-                    Hi, I'm Tommaso and i'm 22 years old.
-                    I live in Italy, more specifically near Bergamo.
-                    I was born in Warsaw (Poland) and i was adopted when i was 5 years old by two italians.
-                </p>
-            </div>
-        </div>
+  <div class="secondSectionContainer">
+    <div class="title">
+      <h2>About me</h2>
     </div>
+    <div class="profile">
+      <img class="profileImg" src="../assets/MyProfilePhoto.jpeg" />
+    </div>
+    <div class="chat">
+        <h3>STORIES</h3>
+
+      <div class="mine messages">
+        <div class="message last">
+            <h4>Personal</h4>
+            <p>
+                Hi, I'm Tommaso and i'm 22 years old.
+                I live in Italy, more specifically near Bergamo.
+                I was born in Warsaw (Poland) and i was adopted when i was 5 years old by two italians.
+            </p>
+        </div>
+      </div>
+      <div class="yours messages">
+        <div class="message last">
+            <h4>School studies and work</h4>
+             <p>
+                Hi, I'm Tommaso and i'm 22 years old.
+                I live in Italy, more specifically near Bergamo.
+                I was born in Warsaw (Poland) and i was adopted when i was 5 years old by two italians.
+            </p>
+        </div>
+      </div>
+      <div class="mine messages">
+        <div class="message last">
+            <h4>Future goals</h4>
+             <p>
+                Hi, I'm Tommaso and i'm 22 years old.
+                I live in Italy, more specifically near Bergamo.
+                I was born in Warsaw (Poland) and i was adopted when i was 5 years old by two italians.
+            </p>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="stories">
+      <div class="borderLeft">
+        <h4>My personal Story</h4>
+        <p>
+          Hi, I'm Tommaso and i'm 22 years old.
+          I live in Italy, more specifically near Bergamo.
+          I was born in Warsaw (Poland) and i was adopted when i was 5 years old by two italians.
+        </p>
+      </div>
+      <div class="textAlignRight borderTop borderRight">
+        <h4>My school studies and work Story</h4>
+        <p>
+          Hi, I'm Tommaso and i'm 22 years old.
+          I live in Italy, more specifically near Bergamo.
+          I was born in Warsaw (Poland) and i was adopted when i was 5 years old by two italians.
+        </p>
+      </div>
+      <div class="borderTop borderLeft">
+        <h4>My future goals Story</h4>
+        <p>
+          Hi, I'm Tommaso and i'm 22 years old.
+          I live in Italy, more specifically near Bergamo.
+          I was born in Warsaw (Poland) and i was adopted when i was 5 years old by two italians.
+        </p>
+      </div>
+    </div> -->
+  </div>
 </template>
 
 <script>
@@ -44,61 +78,164 @@ export default {
 };
 </script>
 <style scoped>
-    .secondSectionContainer {
-        min-height: 70vh;
-        margin: 120px 250px;
-    }
-    .profile {
-        height: 300px;
-        width: 300px;
-        margin:auto;
-    }
-    .profileImg {
-        border-radius: 25px;
-        height: 300px;
-        width: 300px;
-    }
-    .title {
-        display: flex;
-        justify-content: center;
-    }
-    .stories div {
-        padding: 50px;
-    }
-    .textAlignRight {
-        text-align: right;
-    }
-    .borderLeft {
-        border-left: dashed 1px grey;
-        border-radius: 20px;
-    }
-    .borderRight {
-        border-right: dashed 1px grey;
-        border-radius: 20px;
-    }
-    .borderTop {
-        border-top: dashed 1px grey;
-        border-radius: 20px;
-    }
-    .borderBottom {
-        border-bottom: dashed 1px grey;
-        border-radius: 20px;
-    }
-    /* RESPONSIVE */
-    @media (max-width: 767px) {
-        .secondSectionContainer {
-        min-height: 70vh;
-        margin: 0;
-        }
-        .profile {
-            height: 300px;
-            width: 300px;
-            margin:auto;
-        }
-        .profileImg {
-            border-radius: 25px;
-            height: 300px;
-            width: 300px;
-        }
-    }
+h1, h2, h3, h4 {
+  text-transform: uppercase;
+  letter-spacing: 5px;
+}
+.secondSectionContainer {
+  min-height: 70vh;
+  margin: 120px 250px;
+}
+.profile {
+  height: 300px;
+  width: 300px;
+  margin: auto;
+}
+.profileImg {
+  border-radius: 25px;
+  height: 300px;
+  width: 300px;
+}
+.title {
+  display: flex;
+  justify-content: center;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+}
+.stories div {
+  padding: 50px;
+}
+.textAlignRight {
+  text-align: right;
+}
+.borderLeft {
+  border-left: dashed 1px grey;
+  border-radius: 20px;
+}
+.borderRight {
+  border-right: dashed 1px grey;
+  border-radius: 20px;
+}
+.borderTop {
+  border-top: dashed 1px grey;
+  border-radius: 20px;
+}
+.borderBottom {
+  border-bottom: dashed 1px grey;
+  border-radius: 20px;
+}
+/* IPHONE MESSAGES CSS */
+
+.chat {
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding: 10px;
+  margin: auto;
+  margin-top: 100px;
+}
+
+.messages {
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+}
+
+.message {
+  border-radius: 20px;
+  padding: 8px 15px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  display: inline-block;
+}
+
+.yours {
+  align-items: flex-start;
+}
+
+.yours .message {
+  margin-right: 25%;
+  background-color: #eee;
+  position: relative;
+}
+
+.yours .message.last:before {
+  content: "";
+  position: absolute;
+  z-index: 0;
+  bottom: 0;
+  left: -7px;
+  height: 20px;
+  width: 20px;
+  background: #eee;
+  border-bottom-right-radius: 15px;
+}
+.yours .message.last:after {
+  content: "";
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+  left: -10px;
+  width: 10px;
+  height: 20px;
+  background: white;
+  border-bottom-right-radius: 10px;
+}
+
+.mine {
+  align-items: flex-end;
+}
+
+.mine .message {
+  color: white;
+  margin-left: 25%;
+  background: linear-gradient(to bottom, #00d0ea 0%, #0085d1 100%);
+  background-attachment: fixed;
+  position: relative;
+}
+
+.mine .message.last:before {
+  content: "";
+  position: absolute;
+  z-index: 0;
+  bottom: 0;
+  right: -8px;
+  height: 20px;
+  width: 20px;
+  background: linear-gradient(to bottom, #00d0ea 0%, #0085d1 100%);
+  background-attachment: fixed;
+  border-bottom-left-radius: 15px;
+}
+
+.mine .message.last:after {
+  content: "";
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+  right: -10px;
+  width: 10px;
+  height: 20px;
+  background: white;
+  border-bottom-left-radius: 10px;
+}
+
+/* RESPONSIVE */
+@media (max-width: 767px) {
+  .secondSectionContainer {
+    min-height: 70vh;
+    margin: 0;
+  }
+  .profile {
+    height: 300px;
+    width: 300px;
+    margin: auto;
+  }
+  .profileImg {
+    border-radius: 25px;
+    height: 300px;
+    width: 300px;
+  }
+}
 </style>
